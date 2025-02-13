becb1a8b84f9512896877662b1a6de8284c5aec2
Mithi Sevilla <mithi@luxor.tech>
2025-02-14 01:15:09 +0800

    Fix css z-index issue regression

--- src/components/screens/ListScreen/index.tsx
+++ src/components/screens/ListScreen/index.tsx
@@ -4,12 +4,12 @@ import KanjiList from "./sections/KanjiList";
 const ListScreen = () => {
   return (
     <div className="relative">
-      <div className="sticky top-[50px] bg-white dark:bg-black bg-opacity-50 backdrop-blur-sm px-2 pb-2 z-50">
+      <div className="sticky top-[50px] bg-white dark:bg-black bg-opacity-50 backdrop-blur-sm px-2 pb-2 z-40">
         <ControlBar />
       </div>
 
       <div
-        className="overflow-auto relative top-12 z-0"
+        className="overflow-auto relative top-12 -z-0"
         style={{ height: "calc(100svh - 100px)" }}
       >
         <KanjiList />
