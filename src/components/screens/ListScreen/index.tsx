c3afc9927426adacf4db0ce2a8a884fa17f5f392
Mithi Sevilla <mithi@luxor.tech>
2025-02-14 00:29:31 +0800

    Fix css issue

--- src/components/screens/ListScreen/index.tsx
+++ src/components/screens/ListScreen/index.tsx
@@ -4,13 +4,13 @@ import KanjiList from "./sections/KanjiList";
 const ListScreen = () => {
   return (
     <div className="relative">
-      <div className="sticky top-[50px] bg-white dark:bg-black bg-opacity-50 backdrop-blur-sm z-50 px-2 pb-0">
+      <div className="sticky top-[50px] bg-white dark:bg-black bg-opacity-50 backdrop-blur-sm z-0 px-2 pb-2">
         <ControlBar />
       </div>
 
       <div
-        className="overflow-auto relative top-12"
-        style={{ height: "calc(100svh - 20px)" }}
+        className="overflow-auto relative top-12 z-0"
+        style={{ height: "calc(100svh - 100px)" }}
       >
         <KanjiList />
       </div>
