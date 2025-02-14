c8c3c70b21cc0a415c574ad2e8bc96ecb3f58b49
Mithi Sevilla <mithi@luxor.tech>
2025-02-14 21:40:31 +0800

    Hover for light mode darkmode menu

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -8,15 +8,29 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/providers/theme-provider";
+import { useState } from "react";
 
 // Copied from: https://ui.shadcn.com/docs/dark-mode/vite
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <DropdownMenu
+      open={isOpen}
+      onOpenChange={(newState) => {
+        setIsOpen(newState);
+      }}
+    >
+      <DropdownMenuTrigger
+        onMouseEnter={() => {
+          setIsOpen(true);
+        }}
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+      >
         <Button variant="outline" size="icon" className="h-8 w-8">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
