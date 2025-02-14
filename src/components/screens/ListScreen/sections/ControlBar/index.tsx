93179949c56912206e1a726fc4e0f5c786c296c9
Mithi Sevilla <mithi@luxor.tech>
2025-02-14 23:03:56 +0800

    Add wana kana for search input

--- src/components/screens/ListScreen/sections/ControlBar/index.tsx
+++ src/components/screens/ListScreen/sections/ControlBar/index.tsx
@@ -1,7 +1,8 @@
 import { Search } from "lucide-react";
 import { Input } from "../../../../ui/input";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BasicSelect from "@/components/common/BasicSelect";
+import * as wanakana from "wanakana";
 
 import SortAndFilterSettings from "./sections/SortAndFilterSettings";
 import CardPresentationSettings from "./sections/CardPresentationSettings";
@@ -25,11 +26,80 @@ export function SelectSearchInputType({ className }: { className?: string }) {
 }
 
 const SearchInput = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const inputRef = useRef<any>();
+  const [value, setValue] = useState("meaning");
+
+  useEffect(() => {
+    console.log("text", inputRef?.current?.value);
+    const currentValue = inputRef?.current?.value ?? "";
+
+    if (inputRef.current?.value == null) {
+      return;
+    }
+
+    if (value === "meaning") {
+      inputRef.current.value = wanakana.toRomaji(currentValue);
+      try {
+        wanakana.unbind(inputRef.current);
+      } catch {
+        console.log("cannot unbind");
+      }
+      return;
+    }
+
+    if (value === "onyomi") {
+      const newValue = wanakana.toKatakana(currentValue);
+      try {
+        wanakana.unbind(inputRef.current);
+      } catch {
+        console.log("cannot unbind");
+      }
+      inputRef.current.value = newValue;
+      wanakana.bind(inputRef.current, { IMEMode: "toKatakana" });
+      return;
+    }
+
+    if (value === "kunyomi") {
+      const newValue = wanakana.toHiragana(currentValue);
+      try {
+        wanakana.unbind(inputRef.current);
+      } catch {
+        console.log("cannot unbind");
+      }
+      inputRef.current.value = newValue;
+      wanakana.bind(inputRef.current, { IMEMode: "toHiragana" });
+      return;
+    }
+  }, [value]);
+
+  const placeHolder =
+    value === "meaning"
+      ? "Search by Meaning"
+      : value === "onyomi"
+      ? "Search by Onyomi Reading - オニョミ"
+      : "Search by Kunyomi Reading - くにょみ";
   return (
     <section className="w-full relative">
-      <Input className="pl-7 pr-32 h-9" placeholder="Search..." />
+      <Input
+        className="pl-7 pr-32 h-9"
+        placeholder={placeHolder}
+        ref={inputRef}
+      />
       <Search className="pointer-events-none absolute left-2 top-2 size-4 translate-y-0.5 select-none opacity-50" />
-      <SelectSearchInputType />
+      <BasicSelect
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+        triggerCN={
+          "absolute right-1 top-1 w-28 h-7 bg-gray-100 dark:bg-gray-900"
+        }
+        options={[
+          { value: "meaning", label: "Meaning" },
+          { value: "onyomi", label: "Onyomi" },
+          { value: "kunyomi", label: "Kunyomi" },
+        ]}
+        srOnlyLabel="Search Type"
+      />
     </section>
   );
 };
