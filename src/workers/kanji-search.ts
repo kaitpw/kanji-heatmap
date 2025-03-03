dc471c76f2ae0d54db4dad76213cd9c653338150
Michael Galero <4162+mikong@users.noreply.github.com>
2025-03-03 23:47:42 +0800

    feat: Allow secondary sort of any primary sort key
    Implemented by merging the primary and secondary sort logic

--- src/workers/kanji-search.ts
+++ src/workers/kanji-search.ts
@@ -14,6 +14,7 @@ import {
   K_RANK_WIKIPEDIA_DOC,
   K_STROKES,
   K_WK_LVL,
+  SortKey,
 } from "@/lib/frequency-rank";
 import { KanjiExtendedInfo, KanjiMainInfo } from "@/lib/kanji-worker-constants";
 
@@ -73,78 +74,72 @@ export const searchKanji = (settings: SearchSettings, kanjiPool: DataPool) => {
       const exInfoA = kanjiPool.extended[a];
       const exInfoB = kanjiPool.extended[b];
 
-      let compareVal = 0;
-      if (primarySort === K_JLPT) {
-        compareVal = jlptSort(infoA.jlpt, infoB.jlpt);
-      } else if (primarySort === K_JOUYOU_KEY) {
-        compareVal = simpleSort(exInfoA.jouyouGrade, exInfoB.jouyouGrade);
-      } else if (primarySort === K_STROKES) {
-        compareVal = strokeSort(exInfoA.strokes, exInfoB.strokes);
-      } else if (primarySort === K_WK_LVL) {
-        compareVal = simpleSort(exInfoA.wk, exInfoB.wk);
-      } else if (primarySort === K_RANK_NETFLIX) {
-        compareVal = freqSort(
-          exInfoA.frequency?.netflix,
-          exInfoB.frequency?.netflix
-        );
-      } else if (primarySort === K_RANK_DRAMA_SUBTITLES) {
-        compareVal = freqSort(
-          exInfoA.frequency?.dramaSubs,
-          exInfoB.frequency?.dramaSubs
-        );
-      } else if (primarySort === K_RANK_NOVELS_5100) {
-        compareVal = freqSort(
-          exInfoA.frequency?.novels5100,
-          exInfoB.frequency?.novels5100
-        );
-      } else if (primarySort === K_RANK_TWITTER) {
-        compareVal = freqSort(
-          exInfoA.frequency?.twitter,
-          exInfoB.frequency?.twitter
-        );
-      } else if (primarySort === K_RANK_WIKIPEDIA_DOC) {
-        compareVal = freqSort(
-          exInfoA.frequency?.wikiDoc,
-          exInfoB.frequency?.wikiDoc
-        );
-      } else if (primarySort === K_RANK_WIKIPEDIA_CHAR) {
-        compareVal = freqSort(
-          exInfoA.frequency?.wikiChar,
-          exInfoB.frequency?.wikiChar
-        );
-      } else if (primarySort === K_RANK_ONLINE_NEWS_DOC) {
-        compareVal = freqSort(
-          exInfoA.frequency?.onlineNewsDoc,
-          exInfoB.frequency?.onlineNewsDoc
-        );
-      } else if (primarySort === K_RANK_ONLINE_NEWS_CHAR) {
-        compareVal = freqSort(
-          exInfoA.frequency?.onlineNewsChar,
-          exInfoB.frequency?.onlineNewsChar
-        );
-      } else if (primarySort === K_RANK_AOZORA_DOC) {
-        compareVal = freqSort(
-          exInfoA.frequency?.aozoraDoc,
-          exInfoB.frequency?.aozoraDoc
-        );
-      } else if (primarySort === K_RANK_AOZORA_CHAR) {
-        compareVal = freqSort(
-          exInfoA.frequency?.aozoraChar,
-          exInfoB.frequency?.aozoraChar
-        );
-      }
-      if (compareVal != 0) return compareVal;
+      const sortBy = (sortKey: SortKey) => {
+        if (sortKey === K_JLPT) {
+          return jlptSort(infoA.jlpt, infoB.jlpt);
+        } else if (sortKey === K_JOUYOU_KEY) {
+          return simpleSort(exInfoA.jouyouGrade, exInfoB.jouyouGrade);
+        } else if (sortKey === K_STROKES) {
+          return strokeSort(exInfoA.strokes, exInfoB.strokes);
+        } else if (sortKey === K_WK_LVL) {
+          return simpleSort(exInfoA.wk, exInfoB.wk);
+        } else if (sortKey === K_RANK_NETFLIX) {
+          return freqSort(
+            exInfoA.frequency?.netflix,
+            exInfoB.frequency?.netflix
+          );
+        } else if (sortKey === K_RANK_DRAMA_SUBTITLES) {
+          return freqSort(
+            exInfoA.frequency?.dramaSubs,
+            exInfoB.frequency?.dramaSubs
+          );
+        } else if (sortKey === K_RANK_NOVELS_5100) {
+          return freqSort(
+            exInfoA.frequency?.novels5100,
+            exInfoB.frequency?.novels5100
+          );
+        } else if (sortKey === K_RANK_TWITTER) {
+          return freqSort(
+            exInfoA.frequency?.twitter,
+            exInfoB.frequency?.twitter
+          );
+        } else if (sortKey === K_RANK_WIKIPEDIA_DOC) {
+          return freqSort(
+            exInfoA.frequency?.wikiDoc,
+            exInfoB.frequency?.wikiDoc
+          );
+        } else if (sortKey === K_RANK_WIKIPEDIA_CHAR) {
+          return freqSort(
+            exInfoA.frequency?.wikiChar,
+            exInfoB.frequency?.wikiChar
+          );
+        } else if (sortKey === K_RANK_ONLINE_NEWS_DOC) {
+          return freqSort(
+            exInfoA.frequency?.onlineNewsDoc,
+            exInfoB.frequency?.onlineNewsDoc
+          );
+        } else if (sortKey === K_RANK_ONLINE_NEWS_CHAR) {
+          return freqSort(
+            exInfoA.frequency?.onlineNewsChar,
+            exInfoB.frequency?.onlineNewsChar
+          );
+        } else if (sortKey === K_RANK_AOZORA_DOC) {
+          return freqSort(
+            exInfoA.frequency?.aozoraDoc,
+            exInfoB.frequency?.aozoraDoc
+          );
+        } else if (sortKey === K_RANK_AOZORA_CHAR) {
+          return freqSort(
+            exInfoA.frequency?.aozoraChar,
+            exInfoB.frequency?.aozoraChar
+          );
+        }
+        return 0;
+      };
 
-      if (secondarySort === K_JLPT) {
-        return jlptSort(infoA.jlpt, infoB.jlpt);
-      } else if (secondarySort === K_JOUYOU_KEY) {
-        return simpleSort(exInfoA.jouyouGrade, exInfoB.jouyouGrade);
-      } else if (secondarySort === K_STROKES) {
-        return strokeSort(exInfoA.strokes, exInfoB.strokes);
-      } else if (secondarySort === K_WK_LVL) {
-        return simpleSort(exInfoA.wk, exInfoB.wk);
-      }
-      return 0;
+      let compareVal = sortBy(primarySort);
+      if (compareVal != 0) return compareVal;
+      return sortBy(secondarySort);
     });
 
   return kanjiList;
