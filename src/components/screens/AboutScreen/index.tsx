159e21d64dbfa1b752a4d84ba91eda6c8a698007
Mithi Sevilla <mithi@luxor.tech>
2025-03-18 18:58:05 +0800

    Make content screens lazy

--- src/components/screens/AboutScreen/index.tsx
+++ src/components/screens/AboutScreen/index.tsx
@@ -1,3 +1,14 @@
-import AboutScreen from "./AboutScreen";
+import { lazy, Suspense } from "react";
+import { BasicLoading } from "@/components/common/BasicLoading";
+
+const LazyAboutScreen = lazy(() => import("./AboutScreen"));
+
+const AboutScreen = () => {
+  return (
+    <Suspense fallback={<BasicLoading />}>
+      <LazyAboutScreen />
+    </Suspense>
+  );
+};
 
 export { AboutScreen };
--- src/components/screens/DocsScreen/index.tsx
+++ src/components/screens/DocsScreen/index.tsx
@@ -1,3 +1,14 @@
-import DocsScreen from "./DocsScreen";
+import { lazy, Suspense } from "react";
+import { BasicLoading } from "@/components/common/BasicLoading";
+
+const LazyDocsScreen = lazy(() => import("./DocsScreen"));
+
+const DocsScreen = () => {
+  return (
+    <Suspense fallback={<BasicLoading />}>
+      <LazyDocsScreen />
+    </Suspense>
+  );
+};
 
 export { DocsScreen };
