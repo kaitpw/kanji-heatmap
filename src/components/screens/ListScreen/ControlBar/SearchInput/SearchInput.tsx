c2c6b30c3a7319c23d6b552987dc97429425a4bd
Mithi Sevilla <mithi@luxor.tech>
2025-03-23 20:47:54 +0800

    Add clear button on search bar

--- src/components/screens/ListScreen/ControlBar/SearchInput/SearchInput.tsx
+++ src/components/screens/ListScreen/ControlBar/SearchInput/SearchInput.tsx
@@ -9,17 +9,10 @@ import {
 import { translateValue } from "@/lib/wanakana-adapter";
 import { Search } from "@/components/icons";
 import BasicSelect from "@/components/common/BasicSelect";
+import { Button } from "@/components/ui/button";
+import { CircleX } from "lucide-react";
 
 const INPUT_DEBOUNCE_TIME = 1000;
-
-const INPUT_CLASS =
-  "flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm pl-7 pr-[105px] h-9";
-
-const SEARCH_ICON_CLASS =
-  "pointer-events-none absolute left-2 top-2 size-4 translate-y-0.5 select-none opacity-50";
-
-const SELECT_CLASS =
-  "absolute right-1 top-1 w-26 h-7 bg-gray-100 dark:bg-gray-900";
 
 export const SearchInput = ({
   initialSearchType = "keyword",
@@ -44,7 +37,10 @@ export const SearchInput = ({
   return (
     <section className="w-full relative">
       <input
-        className={cn(INPUT_CLASS, fontCN)}
+        className={cn(
+          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm pl-7 pr-[105px] h-9",
+          fontCN
+        )}
         value={parsedValue}
         onChange={(e) => {
           const updatedValue = translateValue(
@@ -63,7 +59,24 @@ export const SearchInput = ({
         placeholder={placeholderMap[searchType]}
       />
 
-      <Search className={cn(SEARCH_ICON_CLASS)} />
+      <Search
+        className={
+          "pointer-events-none absolute left-2 top-2 size-4 translate-y-0.5 select-none opacity-50"
+        }
+      />
+      {parsedValue.length > 0 && (
+        <Button
+          className="absolute right-[110px] top-[6px] m-0 p-1  h-6 rounded-full"
+          variant={"secondary"}
+          onClick={() => {
+            setValue("");
+            onSettle("", searchType);
+          }}
+        >
+          <CircleX />
+          <span className="sr-only"> Clear search text</span>
+        </Button>
+      )}
       <BasicSelect
         value={searchType}
         onChange={(val) => {
@@ -76,7 +89,9 @@ export const SearchInput = ({
           setValue(newParsedValue);
           onSettle(newParsedValue.trim(), newType);
         }}
-        triggerCN={SELECT_CLASS}
+        triggerCN={
+          "absolute right-1 top-1 w-26 h-7 bg-gray-100 dark:bg-gray-900"
+        }
         options={SEARCH_TYPE_OPTIONS}
         label="Search Type"
         isLabelSrOnly={true}
