aa001636c1dda954f04c45eb319c9db1c63d3688
Mithi Sevilla <mithi@luxor.tech>
2025-03-23 17:24:48 +0800

    Fix: wrong path

--- src/lib/kanji/kanji-worker-json-paths.ts
+++ src/lib/kanji/kanji-worker-json-paths.ts
@@ -2,7 +2,7 @@ const jsonPaths = {
   MAIN_KANJI_INFO_FILE_PATH: "/json/kanji_main.json",
   EXTENDED_KANJI_INFO_FILE_PATH: "/json/kanji_extended.json",
   PHONETIC_FILE: "/json/phonetic.json",
-  PART_KEYWORD_FILE: "/json/phonetic.json",
+  PART_KEYWORD_FILE: "/json/component_keyword.json",
   SEGMENTED_VOCAB_FILE: "/json/vocabulary.json",
   CUM_USE: "json/cum_use.json",
 };
